b553a4e48e307e15512e96aec416bc51fa57075c
diyaniliev96 <diyan.iliev96@gmail.com>
2023-10-24 23:25:10 +0300

    Initial commit.

---
+++ src/test/java/com/iliev/domani/DomaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iliev.domani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomaniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
