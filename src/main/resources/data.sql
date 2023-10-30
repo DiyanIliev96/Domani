01f5595de575bec124c8669419e70f7d90a987b8
diyaniliev96 <diyan.iliev96@gmail.com>
2023-10-30 21:39:21 +0200

    Add admin/users page paging.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into users(full_name,email,password) VALUES ('UserFour','user@four.com','secret');
+insert into users(full_name,email,password) VALUES ('UserFive','user@five.com','secret');
+insert into users(full_name,email,password) VALUES ('UserSix','user@six.com','secret');
+insert into users(full_name,email,password) VALUES ('UserSeven','user@seven.com','secret');
+insert into users(full_name,email,password) VALUES ('UserEight','user@eight.com','secret');
+insert into users(full_name,email,password) VALUES ('UserNine','user@nine.com','secret');
+insert into users(full_name,email,password) VALUES ('UserTen','user@ten.com','secret');
