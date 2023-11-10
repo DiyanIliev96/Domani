3aa273d37de72b4a09785dbf7adefa17a628c97c
diyaniliev96 <diyan.iliev96@gmail.com>
2023-11-10 23:54:31 +0200

    Add product entity.

---
+++ src/main/java/com/iliev/domani/model/entity/ProductEntity.java
@@ -0,0 +1,80 @@
+package com.iliev.domani.model.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @ManyToOne(optional = false)
+    private CategoryEntity category;
+    @Column(columnDefinition = "text",nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private BigDecimal price;
+    @Column(nullable = false)
+    private String imageUrl;
+
+    public ProductEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public ProductEntity setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ProductEntity setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public CategoryEntity getCategory() {
+        return category;
+    }
+
+    public ProductEntity setCategory(CategoryEntity category) {
+        this.category = category;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ProductEntity setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public ProductEntity setPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public ProductEntity setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+        return this;
+    }
+}
