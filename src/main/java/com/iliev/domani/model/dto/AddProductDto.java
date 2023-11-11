48a3a928450b87808bea0481645f25835e6433c9
diyaniliev96 <diyan.iliev96@gmail.com>
2023-11-11 13:54:31 +0200

    Add validation in AddProductDto.

--- src/main/java/com/iliev/domani/model/dto/AddProductDto.java
+++ src/main/java/com/iliev/domani/model/dto/AddProductDto.java
@@ -1,20 +1,27 @@
 package com.iliev.domani.model.dto;
 
 import com.iliev.domani.model.enums.CategoryNameEnum;
+import com.iliev.domani.validation.ImageUnder1MB;
+import com.iliev.domani.validation.NotNullImage;
+import jakarta.validation.constraints.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 
 public class AddProductDto {
-
+    @NotBlank
+    @Size(min = 2,max = 100)
     private String name;
 
+    @NotNull(message = "You must select category!")
     private CategoryNameEnum category;
-
+    @NotBlank(message = "Description cannot be blank!")
     private String description;
-
+    @Positive(message = "Price must be positive!")
+    @NotNull(message = "Price cannot be empty!")
     private BigDecimal price;
-
+    @ImageUnder1MB
+    @NotNullImage
     private MultipartFile image;
 
     public AddProductDto() {
