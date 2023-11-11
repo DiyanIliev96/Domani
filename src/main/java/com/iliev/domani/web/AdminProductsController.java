8bbb9e9d647fb4ff92dd410d4358f04062ee82f8
diyaniliev96 <diyan.iliev96@gmail.com>
2023-11-11 14:15:38 +0200

    Add validation for new product.

--- src/main/java/com/iliev/domani/web/AdminProductsController.java
+++ src/main/java/com/iliev/domani/web/AdminProductsController.java
@@ -3,11 +3,14 @@ package com.iliev.domani.web;
 import com.iliev.domani.model.dto.AddProductDto;
 import com.iliev.domani.model.view.ProductView;
 import com.iliev.domani.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.util.List;
@@ -39,8 +42,13 @@ public class AdminProductsController {
     }
 
     @PostMapping("/menu/add")
-    private String addProduct(AddProductDto addProductDto) throws IOException {
-
+    private String addProduct(@Valid AddProductDto addProductDto, BindingResult bindingResult,
+                              RedirectAttributes redirectAttributes) throws IOException {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("newProduct",addProductDto);
+            redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "newProduct",bindingResult);
+            return "redirect:/admin/menu/add";
+        }
         productService.addProduct(addProductDto);
         return "redirect:/admin/menu/management";
     }
