00cdc391b02aac189b1e70a7b7ff0a28f25a4e14
diyaniliev96 <diyan.iliev96@gmail.com>
2023-11-11 15:47:54 +0200

    Add 10 pizzas.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,12 @@
-insert into users(full_name,email,password) VALUES ('UserFour','user@four.com','secret');
-insert into users(full_name,email,password) VALUES ('UserFive','user@five.com','secret');
-insert into users(full_name,email,password) VALUES ('UserSix','user@six.com','secret');
-insert into users(full_name,email,password) VALUES ('UserSeven','user@seven.com','secret');
-insert into users(full_name,email,password) VALUES ('UserEight','user@eight.com','secret');
-insert into users(full_name,email,password) VALUES ('UserNine','user@nine.com','secret');
-insert into users(full_name,email,password) VALUES ('UserTen','user@ten.com','secret');
+insert into products(name,category_id,price,description,image_url)
+VALUES
+    ('Salami pizza','5','5.5','Delicious pizza topped with savory salami slices.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699655341/jkedbgv5ebzr0ffolaym.jpg'),
+    ('Margharita pizza','5','4.5','A classic Italian pizza with fresh tomatoes, mozzarella, and basil.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699705892/mxoq3nhmvpodzro8fgdf.jpg'),
+    ('Hawaiian pizza','5','6.5','Sweet and savory pizza with ham, pineapple, and mozzarella.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699706468/lwjoebiubnfw7iadfhtb.jpg'),
+    ('Supreme pizza','5','7.5','Pizza with a hearty combination of meat and vegetable toppings.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699707873/mpcyl6ig9oadw2ygbhkt.jpg'),
+    ('BBQ Chicken pizza','5','8','Pizza with BBQ sauce, grilled or roasted chicken, and a variety of other toppings.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699708195/frcwsob4y24ccb6l65a5.png'),
+    ('Veggie pizza','5','5','Pizza with a variety of vegetables, such as mushrooms, onions, peppers, and tomatoes.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699708780/lw5b1pboipupeqc7fx9f.jpg'),
+    ('Meat Lovers pizza','5','9','Pizza with a variety of meat toppings, such as pepperoni, sausage, and bacon.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699709073/rt971oje7qwtqlyue8bq.jpg'),
+    ('White pizza','5','6','Pizza made with a white sauce, such as Alfredo or béchamel, instead of tomato sauce.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699709522/uske8ksjssmlwbgjfz8k.jpg'),
+    ('Buffalo Chicken pizza','5','8.5','Spicy chicken pizza with buffalo sauce and mozzarella cheese.','https://res.cloudinary.com/dqboyjgbo/image/upload/v1699709754/ljjicf0bstbrayrqlwxj.jpg'),
+    ('Four Cheese pizza','5','8','Cheesy pizza with four different types of cheese.','https://res.cloudinary.com/dqboyjgbo/image/upload/v1699710209/hav0fpnczncxtumun7xb.jpg');
