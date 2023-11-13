71a4a92a01534a7482983489a96ca3c466f65778
diyaniliev96 <diyan.iliev96@gmail.com>
2023-11-13 21:57:47 +0200

    Change some products images.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 insert into products(name,category_id,price,description,image_url)
 VALUES
     ('Salami pizza','5','5.5','Delicious pizza topped with savory salami slices.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699655341/jkedbgv5ebzr0ffolaym.jpg'),
-    ('Margharita pizza','5','4.5','A classic Italian pizza with fresh tomatoes, mozzarella, and basil.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699705892/mxoq3nhmvpodzro8fgdf.jpg'),
-    ('Hawaiian pizza','5','6.5','Sweet and savory pizza with ham, pineapple, and mozzarella.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699706468/lwjoebiubnfw7iadfhtb.jpg'),
+    ('Margharita pizza','5','4.5','A classic Italian pizza with fresh tomatoes, mozzarella, and basil.','https://res.cloudinary.com/dqboyjgbo/image/upload/v1699904724/plwvrjzqov7tvesqa86r.jpg'),
+    ('Hawaiian pizza','5','6.5','Sweet and savory pizza with ham, pineapple, and mozzarella.','https://res.cloudinary.com/dqboyjgbo/image/upload/v1699904518/uoif4ghru7gmzli64hoc.jpg'),
     ('Supreme pizza','5','7.5','Pizza with a hearty combination of meat and vegetable toppings.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699707873/mpcyl6ig9oadw2ygbhkt.jpg'),
     ('BBQ Chicken pizza','5','8','Pizza with BBQ sauce, grilled or roasted chicken, and a variety of other toppings.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699708195/frcwsob4y24ccb6l65a5.png'),
     ('Veggie pizza','5','5','Pizza with a variety of vegetables, such as mushrooms, onions, peppers, and tomatoes.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699708780/lw5b1pboipupeqc7fx9f.jpg'),
@@ -14,7 +14,7 @@ VALUES
 # Insert breakfasts
 ('Scrambled eggs','1','4','Eggs cooked quickly and stirred until they form soft curds.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699901422/jjocnnazxsqxuijzhc1k.jpg'),
       ('Pancakes','1','4.5','Flat, round cake made from batter and cooked on a hot surface.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699901696/p001909iw1tlgtph5n05.jpg'),
-      ('Bacon and eggs','1','5.5','Bacon and eggs cooked together.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699901956/qnirqsegfkbt6fxbu7yb.jpg'),
+      ('Bacon and eggs','1','5.5','Bacon and eggs cooked together.','https://res.cloudinary.com/dqboyjgbo/image/upload/v1699904241/esmey3qvij1lvtx4skja.jpg'),
     ('Oatmeal','1','4.3','Boiled or steamed groats of oats.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699902178/blr245xvcsuponzvscg1.jpg'),
     ('Continental breakfast','1','7','Light breakfast with pastries, fruits, toast, and coffee.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699902360/dspr4tyrlqbutju881fu.jpg'),
     ('Breakfast Smoothie','1','5','Smoothie made with mixed berries, banana, yogurt, and milk.','https://res.cloudinary.com/dqboyjgbo/image/upload/v1699902587/mkjalokeyveaulx463oj.jpg'),
