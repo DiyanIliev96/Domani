5b7c232cd59db8b6f16b1b7c9f2aa9f356349d31
diyaniliev96 <diyan.iliev96@gmail.com>
2023-11-13 21:28:55 +0200

    Add 10 breakfasts to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,4 +9,17 @@ VALUES
     ('Meat Lovers pizza','5','9','Pizza with a variety of meat toppings, such as pepperoni, sausage, and bacon.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699709073/rt971oje7qwtqlyue8bq.jpg'),
     ('White pizza','5','6','Pizza made with a white sauce, such as Alfredo or béchamel, instead of tomato sauce.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699709522/uske8ksjssmlwbgjfz8k.jpg'),
     ('Buffalo Chicken pizza','5','8.5','Spicy chicken pizza with buffalo sauce and mozzarella cheese.','https://res.cloudinary.com/dqboyjgbo/image/upload/v1699709754/ljjicf0bstbrayrqlwxj.jpg'),
-    ('Four Cheese pizza','5','8','Cheesy pizza with four different types of cheese.','https://res.cloudinary.com/dqboyjgbo/image/upload/v1699710209/hav0fpnczncxtumun7xb.jpg');
+    ('Four Cheese pizza','5','8','Cheesy pizza with four different types of cheese.','https://res.cloudinary.com/dqboyjgbo/image/upload/v1699710209/hav0fpnczncxtumun7xb.jpg'),
+
+# Insert breakfasts
+('Scrambled eggs','1','4','Eggs cooked quickly and stirred until they form soft curds.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699901422/jjocnnazxsqxuijzhc1k.jpg'),
+      ('Pancakes','1','4.5','Flat, round cake made from batter and cooked on a hot surface.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699901696/p001909iw1tlgtph5n05.jpg'),
+      ('Bacon and eggs','1','5.5','Bacon and eggs cooked together.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699901956/qnirqsegfkbt6fxbu7yb.jpg'),
+    ('Oatmeal','1','4.3','Boiled or steamed groats of oats.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699902178/blr245xvcsuponzvscg1.jpg'),
+    ('Continental breakfast','1','7','Light breakfast with pastries, fruits, toast, and coffee.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699902360/dspr4tyrlqbutju881fu.jpg'),
+    ('Breakfast Smoothie','1','5','Smoothie made with mixed berries, banana, yogurt, and milk.','https://res.cloudinary.com/dqboyjgbo/image/upload/v1699902587/mkjalokeyveaulx463oj.jpg'),
+    ('Avocado Toast','1','4.8','Smashed avocado on toast, topped with desired toppings.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699902901/hizhvmdcu4qurqbl1qxq.jpg'),
+    ('Bagels with cream cheese and salmon','1','8','Sliced bagel spread with cream cheese and topped with smoked salmon.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699903046/ri2308jpubbpzsfjvbqg.jpg'),
+    ('Cereal','1','3.5','Processed grains typically eaten with milk.','https://res.cloudinary.com/dqboyjgbo/image/upload/v1699903235/ln0nrvogtbsgkkxcathv.jpg'),
+    ('Chia Pudding','1','3','Pudding made with chia seeds, milk, and sweetener.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699903512/kt1h09hlgntm9ezofuul.jpg');
+
