4079feac534fa9c0c4a34e8fce2dc98b9a86aaf4
diyaniliev96 <diyan.iliev96@gmail.com>
2023-11-16 13:46:15 +0200

    Add 10 lunch products.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,5 +21,17 @@ VALUES
     ('Avocado Toast','1','4.8','Smashed avocado on toast, topped with desired toppings.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699902901/hizhvmdcu4qurqbl1qxq.jpg'),
     ('Bagels with cream cheese and salmon','1','8','Sliced bagel spread with cream cheese and topped with smoked salmon.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699903046/ri2308jpubbpzsfjvbqg.jpg'),
     ('Cereal','1','3.5','Processed grains typically eaten with milk.','https://res.cloudinary.com/dqboyjgbo/image/upload/v1699903235/ln0nrvogtbsgkkxcathv.jpg'),
-    ('Chia Pudding','1','3','Pudding made with chia seeds, milk, and sweetener.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699903512/kt1h09hlgntm9ezofuul.jpg');
+    ('Chia Pudding','1','3','Pudding made with chia seeds, milk, and sweetener.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1699903512/kt1h09hlgntm9ezofuul.jpg'),
+
+#     Insert lunches
+    ('Grilled cheese sandwich','2','5.3','Two slices of bread and cheese, grilled until the cheese is melted and the bread is golden brown.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1700133140/gbyaangnvpd69w5eriyj.jpg'),
+    ('Tuna salad sandwich','2','6','Tuna, mayonnaise, celery, and onion, served on bread or a croissant.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1700133283/qm4safpapua6tjvq2qow.jpg'),
+    ('Chicken salad sandwich','2','4.5','Cooked chicken, mayonnaise, celery, and onion, served on bread or a croissant.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1700133403/dntbynemyy9ua0npiges.jpg'),
+    ('BLT sandwich','2','6','Bacon, lettuce, and tomato, served on bread or a croissant.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1700133542/ualmzve2rytwlkosbvyu.jpg'),
+    ('Ham and cheese sandwich','2','5.2','Ham, cheese, and bread, served with a side of chips or salad.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1700133673/ksppfgbtqocno5ue3mh7.jpg'),
+    ('Soup and salad','2','7','A classic combination with endless possibilities.','https://res.cloudinary.com/dqboyjgbo/image/upload/v1700133800/eknytrl58x8nxq4hpc3b.jpg'),
+    ('Wrap','2','6.2','Portable and easy-to-eat, filled with a variety of ingredients.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1700134013/wdoqex3nqomc9wx58aft.jpg'),
+    ('Salad','2','5','Healthy and refreshing, made with a variety of ingredients.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1700134116/zbwytxzyk3fcvmpmqhjr.jpg'),
+    ('Quinoa bowl','2','5.6','Trendy and nutritious, packed with protein and fiber.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1700134235/zsx622bo3if97lf3mowz.jpg'),
+    ('Pasta salad','2','5.7','Pasta and vegetables tossed in a dressing.','http://res.cloudinary.com/dqboyjgbo/image/upload/v1700134460/vtqf13jbjhsw3r8ba64f.jpg');
 
