8f959ccd92c212d2111045302dce4131fa160157
diyaniliev96 <diyan.iliev96@gmail.com>
2023-11-20 17:37:57 +0200

    Add custom error messages for wrong input types in BookingDto fields - bookingDateTime,phoneNumber,numberOfGuests.

---
+++ src/main/java/com/iliev/domani/config/MessagesSource.java
@@ -0,0 +1,17 @@
+package com.iliev.domani.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class MessagesSource {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        return messageSource;
+    }
+}
