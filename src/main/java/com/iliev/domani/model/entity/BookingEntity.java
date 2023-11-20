34a0d7d9644fe4b982ece64fcc72c03023a0168d
diyaniliev96 <diyan.iliev96@gmail.com>
2023-11-20 13:18:49 +0200

    Create booking entity.

---
+++ src/main/java/com/iliev/domani/model/entity/BookingEntity.java
@@ -0,0 +1,73 @@
+package com.iliev.domani.model.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "bookings")
+public class BookingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+    @Column(nullable = false)
+    private String phoneNumber;
+    @Column(nullable = false)
+    private LocalDateTime bookingDateTime;
+
+    @Column(nullable = false)
+    private int numberOfGuests;
+
+    public BookingEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public BookingEntity setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public BookingEntity setFullName(String fullName) {
+        this.fullName = fullName;
+        return this;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public BookingEntity setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public LocalDateTime getBookingDateTime() {
+        return bookingDateTime;
+    }
+
+    public BookingEntity setBookingDateTime(LocalDateTime bookingDateTime) {
+        this.bookingDateTime = bookingDateTime;
+        return this;
+    }
+
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public BookingEntity setNumberOfGuests(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+        return this;
+    }
+}
