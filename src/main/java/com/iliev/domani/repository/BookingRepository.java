7abc2ff69d52462617b357e943aed3a6c8b24af9
diyaniliev96 <diyan.iliev96@gmail.com>
2023-12-01 15:27:47 +0200

    Add query that retrieve expired bookings.

--- src/main/java/com/iliev/domani/repository/BookingRepository.java
+++ src/main/java/com/iliev/domani/repository/BookingRepository.java
@@ -3,5 +3,10 @@ package com.iliev.domani.repository;
 import com.iliev.domani.model.entity.BookingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<BookingEntity,Long> {
+
+    List<BookingEntity> findAllByBookingDateTimeBefore(LocalDateTime bookingDateTime);
 }
