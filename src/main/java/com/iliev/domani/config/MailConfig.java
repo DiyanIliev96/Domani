317cf928d956c91f1410ae397ce48b568edecfb4
diyaniliev96 <diyan.iliev96@gmail.com>
2023-12-02 22:24:27 +0200

    Add java mail sender configure.

---
+++ src/main/java/com/iliev/domani/config/MailConfig.java
@@ -0,0 +1,35 @@
+package com.iliev.domani.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender(
+            @Value("${mail.username}") String mailUsername,
+            @Value("${mail_password}") String password
+    ) {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(587);
+        javaMailSender.setUsername(mailUsername);
+        javaMailSender.setPassword(password);
+        javaMailSender.setJavaMailProperties(mailProperties());
+        return javaMailSender;
+    }
+
+    private Properties mailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.smtp.auth","true");
+        properties.setProperty("mail.transport.protocol","smtp");
+        properties.put("mail.smtp.starttls.enable", "true");
+        return properties;
+    }
+}
