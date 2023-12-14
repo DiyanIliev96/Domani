1640c60ff2d1e39b5863bc9054a22afee8d1fc1b
diyaniliev96 <diyan.iliev96@gmail.com>
2023-12-14 15:18:06 +0200

    Registry second interceptor.

--- src/main/java/com/iliev/domani/config/WebConfig.java
+++ src/main/java/com/iliev/domani/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.iliev.domani.config;
 
 import com.iliev.domani.interceptor.BookingInterceptor;
+import com.iliev.domani.interceptor.BookingNext1HourInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final BookingInterceptor bookingInterceptor;
+    private final BookingNext1HourInterceptor bookingNext1HourInterceptor;
 
-    public WebConfig(BookingInterceptor bookingInterceptor) {
+    public WebConfig(BookingInterceptor bookingInterceptor, BookingNext1HourInterceptor bookingNext1HourInterceptor) {
         this.bookingInterceptor = bookingInterceptor;
+        this.bookingNext1HourInterceptor = bookingNext1HourInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(bookingInterceptor);
+        registry.addInterceptor(bookingNext1HourInterceptor);
     }
 }
