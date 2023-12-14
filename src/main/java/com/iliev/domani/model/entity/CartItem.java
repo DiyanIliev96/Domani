8031bb242f09241f916062b2fb3e02686e5085c4
diyaniliev96 <diyan.iliev96@gmail.com>
2023-12-14 15:22:06 +0200

    Create CartItem entity.

---
+++ src/main/java/com/iliev/domani/model/entity/CartItem.java
@@ -0,0 +1,75 @@
+package com.iliev.domani.model.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private UserEntity user;
+    @ManyToOne
+    private ProductEntity product;
+    @Column
+    private int quantity;
+    @Column
+    private BigDecimal totalPrice;
+
+    public CartItem() {
+        this.totalPrice = this.setTotalPrice().getTotalPrice();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public CartItem setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public ProductEntity getProduct() {
+        return product;
+    }
+
+    public CartItem setProduct(ProductEntity product) {
+        this.product = product;
+        return this;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public CartItem setQuantity(int quantity) {
+        this.quantity = quantity;
+        return this;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public CartItem setTotalPrice() {
+        if (getProduct() == null) {
+            return this;
+        }
+        this.totalPrice = this.getProduct().getPrice().multiply(BigDecimal.valueOf(this.getQuantity()));
+        return this;
+    }
+
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public CartItem setUser(UserEntity user) {
+        this.user = user;
+        return this;
+    }
+}
