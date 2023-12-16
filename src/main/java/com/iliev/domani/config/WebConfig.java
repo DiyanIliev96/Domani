9c6f3f398d74dbe501c8d08916e0dcfecb9c8ea3
diyaniliev96 <diyan.iliev96@gmail.com>
2023-12-17 00:14:43 +0200

    Add and registry BookingIsAuthenticatedInterceptor.

--- src/main/java/com/iliev/domani/config/WebConfig.java
+++ src/main/java/com/iliev/domani/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.iliev.domani.config;
 
-import com.iliev.domani.interceptor.BookingInterceptor;
+import com.iliev.domani.interceptor.BookingIsAuthenticatedInterceptor;
+import com.iliev.domani.interceptor.BookingFridayInterceptor;
 import com.iliev.domani.interceptor.BookingNext1HourInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -8,17 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    private final BookingInterceptor bookingInterceptor;
+    private final BookingFridayInterceptor bookingFridayInterceptor;
     private final BookingNext1HourInterceptor bookingNext1HourInterceptor;
 
-    public WebConfig(BookingInterceptor bookingInterceptor, BookingNext1HourInterceptor bookingNext1HourInterceptor) {
-        this.bookingInterceptor = bookingInterceptor;
+    private final BookingIsAuthenticatedInterceptor bookingIsAuthenticatedInterceptor;
+
+    public WebConfig(BookingFridayInterceptor bookingFridayInterceptor, BookingNext1HourInterceptor bookingNext1HourInterceptor, BookingIsAuthenticatedInterceptor bookingIsAuthenticatedInterceptor) {
+        this.bookingFridayInterceptor = bookingFridayInterceptor;
         this.bookingNext1HourInterceptor = bookingNext1HourInterceptor;
+        this.bookingIsAuthenticatedInterceptor = bookingIsAuthenticatedInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(bookingInterceptor);
+        registry.addInterceptor(bookingFridayInterceptor);
         registry.addInterceptor(bookingNext1HourInterceptor);
+        registry.addInterceptor(bookingIsAuthenticatedInterceptor);
     }
 }
