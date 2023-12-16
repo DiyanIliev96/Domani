2c21ab1689e7f837c317c582d020420210d51a86
diyaniliev96 <diyan.iliev96@gmail.com>
2023-12-17 00:18:34 +0200

    Add send booking email method.

--- src/main/java/com/iliev/domani/service/EmailService.java
+++ src/main/java/com/iliev/domani/service/EmailService.java
@@ -27,16 +27,37 @@ public class EmailService {
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
             mimeMessageHelper.setTo(userEmail);
             mimeMessageHelper.setSubject("Welcome!");
-            mimeMessageHelper.setText(generateMessageContent(userFullName),true);
+            mimeMessageHelper.setText(generateMessageContentRegistration(userFullName),true);
             javaMailSender.send(mimeMessageHelper.getMimeMessage());
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private String generateMessageContent(String fullName) {
+    private String generateMessageContentRegistration(String fullName) {
         Context ctx = new Context();
         ctx.setVariable("fullName",fullName);
      return  templateEngine.process("email/welcome",ctx);
+    }
+
+    public void sendBookingEmail(String userEmail,String userFullName,String bookingDateTime) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            mimeMessageHelper.setTo(userEmail);
+            mimeMessageHelper.setSubject("Booking!");
+            mimeMessageHelper.setText(generateMessageContentBooking(userFullName,bookingDateTime),true);
+            javaMailSender.send(mimeMessageHelper.getMimeMessage());
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String generateMessageContentBooking(String userFullName,String bookingDateTime) {
+        Context ctx = new Context();
+        ctx.setVariable("userFullName",userFullName);
+        ctx.setVariable("bookingDateTime",bookingDateTime);
+        return templateEngine.process("email/booking-email",ctx);
     }
 }
