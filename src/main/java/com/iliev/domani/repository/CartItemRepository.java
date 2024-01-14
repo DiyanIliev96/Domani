d395f7017abeac445e707b9f05b0ef0848a1d4af
diyaniliev96 <diyan.iliev96@gmail.com>
2024-01-14 19:55:36 +0200

    Add to CartItemRepository query to find all by order id.

--- src/main/java/com/iliev/domani/repository/CartItemRepository.java
+++ src/main/java/com/iliev/domani/repository/CartItemRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
     List<CartItem> findAllByUser_idAndOrderIsNull(Long user_id);
     void deleteAllByUser_Id(Long user_id);
+    List<CartItem> findAllByOrder_Id(long order_id);
 }
