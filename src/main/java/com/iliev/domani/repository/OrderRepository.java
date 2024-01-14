cbd809f4cfeef50518fe22d3f06021ce50e48388
diyaniliev96 <diyan.iliev96@gmail.com>
2024-01-14 19:54:58 +0200

    Add to OrderRepository query that find all by order status and order by order date and time descending.

--- src/main/java/com/iliev/domani/repository/OrderRepository.java
+++ src/main/java/com/iliev/domani/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package com.iliev.domani.repository;
 
 import com.iliev.domani.model.entity.OrderEntity;
+import com.iliev.domani.model.enums.OrderStatusName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<OrderEntity,Long> {
+    List<OrderEntity> findAllByOrderStatusOrderByOrderDateTimeDesc(OrderStatusName orderStatus);
 }
