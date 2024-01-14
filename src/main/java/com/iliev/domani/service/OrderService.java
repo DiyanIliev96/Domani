64f31828c36307f54ba213fe2ed9b218522284ad
diyaniliev96 <diyan.iliev96@gmail.com>
2024-01-14 13:46:46 +0200

    Add functionality user to make order.

--- src/main/java/com/iliev/domani/service/OrderService.java
+++ src/main/java/com/iliev/domani/service/OrderService.java
@@ -1,14 +1,50 @@
 package com.iliev.domani.service;
 
+import com.iliev.domani.exception.ObjectNotFoundException;
+import com.iliev.domani.model.dto.OrderDto;
+import com.iliev.domani.model.entity.CartItem;
+import com.iliev.domani.model.entity.OrderEntity;
+import com.iliev.domani.model.entity.UserEntity;
+import com.iliev.domani.model.enums.OrderStatusName;
+import com.iliev.domani.repository.CartItemRepository;
 import com.iliev.domani.repository.OrderRepository;
+import com.iliev.domani.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
 
-    public OrderService(OrderRepository orderRepository) {
+    private final CartItemRepository cartItemRepository;
+
+    public OrderService(OrderRepository orderRepository, UserRepository userRepository, CartItemRepository cartItemRepository) {
         this.orderRepository = orderRepository;
+        this.userRepository = userRepository;
+        this.cartItemRepository = cartItemRepository;
+    }
+
+    public void makeOrder(OrderDto orderDto, Long userId) {
+        UserEntity user = userRepository
+                .findById(userId)
+                .orElseThrow(() -> new ObjectNotFoundException("User with id " + userId + " not found.Exception in makeOrder method."));
+        OrderEntity newOrder = new OrderEntity()
+                .setOrderDateTime(LocalDateTime.now())
+                .setUser(user)
+                .setDeliveryAddress(orderDto.getDeliveryAddress())
+                .setPhoneNumber(orderDto.getPhoneNumber().toString())
+                .setAdditionalInfo(orderDto.getAdditionalInfo())
+                .setOrderStatus(OrderStatusName.PENDING);
+
+        orderRepository.save(newOrder);
+
+        List<CartItem> userCartItems = cartItemRepository.findAllByUser_idAndOrderIsNull(userId);
+
+        userCartItems.forEach(cartItem -> cartItem.setOrder(newOrder));
+        cartItemRepository.saveAll(userCartItems);
     }
 }
--- src/main/java/com/iliev/domani/web/OrderController.java
+++ src/main/java/com/iliev/domani/web/OrderController.java
@@ -1,16 +1,49 @@
 package com.iliev.domani.web;
 
+import com.iliev.domani.model.dto.OrderDto;
+import com.iliev.domani.service.CartItemService;
 import com.iliev.domani.service.OrderService;
+import com.iliev.domani.user.DomaniUserDetail;
+import jakarta.validation.Valid;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class OrderController {
 
     private final OrderService orderService;
+    private final CartItemService cartItemService;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, CartItemService cartItemService) {
         this.orderService = orderService;
+        this.cartItemService = cartItemService;
     }
 
 
+    @GetMapping("/order")
+    public String getOrder(@AuthenticationPrincipal DomaniUserDetail domaniUserDetail, Model model) {
+        if (cartItemService.getItemsByUserId(domaniUserDetail.getId()).isEmpty()) {
+            return "redirect:/cart";
+        }
+
+        if (!model.containsAttribute("newOrder")) {
+            model.addAttribute("newOrder",new OrderDto());
+        }
+
+        return "order";
+    }
+
+    @PostMapping("/order")
+    public String makeOrder(@Valid OrderDto orderDto, BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes,
+                            @AuthenticationPrincipal DomaniUserDetail domaniUserDetail) {
+
+        orderService.makeOrder(orderDto,domaniUserDetail.getId());
+        return "redirect:/cart";
+    }
 }
