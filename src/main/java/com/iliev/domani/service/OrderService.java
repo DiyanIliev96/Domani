413d4391b99c22829ca3d10ace065c753398ecdb
diyaniliev96 <diyan.iliev96@gmail.com>
2024-01-14 19:57:54 +0200

    Add functionality admin to view,approve and delete orders.

--- src/main/java/com/iliev/domani/service/OrderService.java
+++ src/main/java/com/iliev/domani/service/OrderService.java
@@ -6,9 +6,13 @@ import com.iliev.domani.model.entity.CartItem;
 import com.iliev.domani.model.entity.OrderEntity;
 import com.iliev.domani.model.entity.UserEntity;
 import com.iliev.domani.model.enums.OrderStatusName;
+import com.iliev.domani.model.view.CartItemView;
+import com.iliev.domani.model.view.OrderView;
 import com.iliev.domani.repository.CartItemRepository;
 import com.iliev.domani.repository.OrderRepository;
 import com.iliev.domani.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -22,10 +26,13 @@ public class OrderService {
 
     private final CartItemRepository cartItemRepository;
 
-    public OrderService(OrderRepository orderRepository, UserRepository userRepository, CartItemRepository cartItemRepository) {
+    private final ModelMapper modelMapper;
+
+    public OrderService(OrderRepository orderRepository, UserRepository userRepository, CartItemRepository cartItemRepository, ModelMapper modelMapper) {
         this.orderRepository = orderRepository;
         this.userRepository = userRepository;
         this.cartItemRepository = cartItemRepository;
+        this.modelMapper = modelMapper;
     }
 
     public void makeOrder(OrderDto orderDto, Long userId) {
@@ -46,5 +53,44 @@ public class OrderService {
 
         userCartItems.forEach(cartItem -> cartItem.setOrder(newOrder));
         cartItemRepository.saveAll(userCartItems);
+    }
+
+    public List<OrderView> getAllOrdersStatusPending() {
+
+        List<OrderEntity> allByOrderStatus = orderRepository.findAllByOrderStatusOrderByOrderDateTimeDesc(OrderStatusName.PENDING);
+        List<OrderView> orderViews = allByOrderStatus.stream()
+                .map(order -> modelMapper.map(order, OrderView.class))
+                .toList();
+
+        orderViews.forEach(orderView -> {
+            String substringDate = orderView.getOrderDateTime().substring(0,orderView.getOrderDateTime().indexOf("."));
+            String viewDate = substringDate.replace("T", " at ");
+            orderView.setOrderDateTime(viewDate);
+        });
+
+        return orderViews;
+    }
+    @Transactional
+    public void deleteOrder(Long id, Long userId) {
+        List<CartItem> allByOrderId = cartItemRepository.findAllByOrder_Id(id);
+        allByOrderId.forEach(cartItem -> cartItem.setOrder(null));
+        cartItemRepository.saveAll(allByOrderId);
+        cartItemRepository.deleteAllByUser_Id(userId);
+        orderRepository.deleteById(id);
+    }
+
+    public void approveOrder(Long orderId) {
+        OrderEntity order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ObjectNotFoundException("Order with id " + orderId +
+                        " not found.Error in approveOrder method."));
+        order.setOrderStatus(OrderStatusName.APPROVED);
+        orderRepository.save(order);
+    }
+
+    public List<CartItemView> getOrderItemsDetails(Long orderId) {
+       return cartItemRepository.findAllByOrder_Id(orderId)
+               .stream()
+               .map(cartItem -> modelMapper.map(cartItem, CartItemView.class))
+               .toList();
     }
 }
